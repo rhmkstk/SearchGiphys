a7bd906f0b35f2bce5ebf041dee4c836dd53bfdc
Rahmi Kostik <rahmi.kstk@gmail.com>
2019-12-18 13:15:07 +0300

    server created

---
+++ build/precache-manifest.48391587f45f83ddbc2de33b12e7e41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f527cb7926708a158438239adc84413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a4f8e03c69e8d77a0e",
+    "url": "/static/css/main.b324b2be.chunk.css"
+  },
+  {
+    "revision": "a4f6a984639dcb23d953",
+    "url": "/static/js/2.89d46aaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.89d46aaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0a4f8e03c69e8d77a0e",
+    "url": "/static/js/main.05e0ee97.chunk.js"
+  },
+  {
+    "revision": "839a1ef5c2ffd448fc42",
+    "url": "/static/js/runtime-main.e4cbe303.js"
+  }
+]);
